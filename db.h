f35355100fa071c6a29a6d14e6ac0b745c029465
Holger Schinzel <holger.schinzel@gmx.de>
2015-01-20 07:47:47 +0100

    update to Darkcoin Core 0.11.0.x

--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 70051
+#define REQUIRE_VERSION 70053
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 168000;
+    return testnet ? 0 : 206150;
 }
 
 std::string static inline ToString(const CService &ip) {
