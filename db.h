3039f894cbed81638f8d0fd88e56bf3a4aa070c6
Holger Schinzel <holger.schinzel@gmx.de>
2015-02-10 14:23:21 +0100

    update to 0.11.1.x

--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 70053
+#define REQUIRE_VERSION 70066
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 206150;
+    return testnet ? 0 : 217752;
 }
 
 std::string static inline ToString(const CService &ip) {
