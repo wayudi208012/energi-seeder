5a842ff8f3d6efaf7f38f1cc74f46371868e417b
Holger Schinzel <holger.schinzel@gmx.de>
2015-02-12 17:11:50 +0100

    checkpoint

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 217752;
+    return testnet ? 0 : 218965;
 }
 
 std::string static inline ToString(const CService &ip) {
