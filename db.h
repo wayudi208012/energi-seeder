12df7847203c279da9587614c86fc0714fd310fd
Holger Schinzel <holger.schinzel@gmx.de>
2015-04-10 13:10:53 +0200

    updated blockcount

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 244000;
+    return testnet ? 0 : 250000;
 }
 
 std::string static inline ToString(const CService &ip) {
