37be9d29aee7c94a006177242ddb51f54bc7f2f9
Ryan Lucchese <ryan@energi.team>
2018-02-07 07:20:14 -0700

    set requireheight to 0 for now

--- db.h
+++ db.h
@@ -16,7 +16,8 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 127606 : 632000;
+  // TODO: eventually update me with a chainheight > 0
+  return testnet ? 0 : 0;
 }
 
 std::string static inline ToString(const CService &ip) {
